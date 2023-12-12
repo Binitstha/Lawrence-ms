e3dea2abd7e0aa4e1f19b21bf0f82b97a5594140
Binitstha <binitcr336@gmail.com>
2023-12-12 22:45:56 +0545

    internal marks complete

--- internalmarks/index.js
+++ internalmarks/index.js
@@ -1,46 +1,82 @@
-const submitBtn = document.getElementById("submitBtn")
-const student = document.querySelectorAll(".std-name")
-const assignmentmodel = document.getElementById("assignment-view-modal")
-
-const attendanceInput = document.getElementById("attandenceInput")
-const labworksInput = document.getElementById("labworksInput")
-const assignmentInput = document.getElementById("assignmentInput")
-const submitMarksBtn = document.getElementById("marksSubmit")
-const x = document.getElementById('ass-model-close-btn')
-
-student.forEach(el => {
-    el.addEventListener("click", () => {
-        assignmentmodel.style.top = "0vh"
-        assignmentmodel.style.transition = "1s ease-in ease-out"
-    })
+const submitBtn = document.getElementById("submitBtn");
+const assignmentmodel = document.getElementById("assignment-view-modal");
+const attendanceInput = document.getElementById("attandenceInput");
+const labworksInput = document.getElementById("labworksInput");
+const assignmentInput = document.getElementById("assignmentInput");
+const submitMarksBtn = document.getElementById("marksSubmit");
+const x = document.getElementById("ass-model-close-btn");
+const student = document.querySelectorAll(".std-name");
+const studentNameRender = document.getElementById("studentName");
+studentNameRender.addEventListener("click", (el) => {
+    const clickedElement = el.target.closest(".std-name");
+    assignmentmodel.style.top = "0vh";
+    assignmentmodel.style.transition = "1s ease-in ease-out";
 });
 
 x.addEventListener("click", () => {
-    assignmentmodel.style.top = "-100vh"
+    assignmentmodel.style.top = "-100vh";
+});
+
+fetch("http://localhost:3000/studentData", {
+    method: "GET",
+    headers: {
+        "content-type": "application/json",
+    },
 })
-
-
-submitMarksBtn.addEventListener('click', () => {
-    const marks = Number(attendanceInput.value) + Number(labworksInput.value) + Number(assignmentInput.value)
-
-    const status = ()=>{
-        return {marks,studentid}
-    }
-    fetch('http://localhost:3000/internalmark', {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-        },
-        body: JSON.stringify(marks)
+    .then((response) => {
+        return response.json();
     })
-        .then((response) => {
-            if (!response.ok) {
-                throw "Response Error"
-            }
-            return response.json;
-        }).then((data) => {
-            console.log(data);
-        }).catch((error) => {
-            console.log(error);
+    .then((data) => {
+        data.studentNames.forEach((el) => {
+            const listItem = document.createElement("ul");
+            listItem.innerHTML = `<div class="std-name rounded-lg shadow-lg bg-white mt-1 p-2 w-max min-w-[18.2rem]">
+        <div class="font-bold text-2xl">
+        <p>${el}</p>
+        </div>`;
+            studentNameRender.appendChild(listItem);
+        });
+        console.log("Fetched");
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+submitMarksBtn.addEventListener("click", () => {
+    const calculateMarks =
+        Number(attendanceInput.value) +
+        Number(labworksInput.value) +
+        Number(assignmentInput.value);
+    const status = {
+        "marks": calculateMarks,
+        "studentid": 102,
+        "semester": "1st",
+        "remark": "Fail"
+    };
+    try {
+
+        fetch("http://localhost:3000/internalMarks", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(status)
         })
-})
+            .then((response) => {
+                if (!response.ok) {
+                    throw "Response Error";
+                }
+                else {
+                    console.log("Data inserted")
+                }
+                return response.json;
+            })
+            .then((data) => {
+                return data;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    } catch (err) {
+        console.log("err")
+    }
+});
