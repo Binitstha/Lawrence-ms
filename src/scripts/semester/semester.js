887f777dfbc989ae4612d6d2b161b3ed385ac765
SCHT001 <sachetsubedi001@gmail.com>
2024-02-08 15:38:04 +0545

    subject display ui changes

--- src/scripts/semester/semester.js
+++ src/scripts/semester/semester.js
@@ -1,4 +1,4 @@
-export const chapterName = [
+export const semesterDetails = [
     {
         semesterId: "1",
         subjects: [
--- src/scripts/subject/subjectsRenderer.js
+++ src/scripts/subject/subjectsRenderer.js
@@ -1,145 +1,146 @@
-const subjects = [
-	{
-		id: 1,
-		semSubs: [
-			{
-				SubjectName: [
-					"Physics",
-					"Digital logic",
-					"Mathematics I",
-					"Introduction of Information and technology",
-					"C programming",
-				],
-				ChapterName: [
-					"1Chapter1",
-					"1chapter2",
-					"1chapter3",
-					"1chapter4",
-					"1chapter5",
-				],
-			},
-		],
-	},
-	{
-		id: 2,
-		semSubs: [
-			{
-				SubjectName: ["Microprocessor", "sub2", "sub2", "sub2", "sub2", "sub2"],
-				ChapterName: [
-					"1Chapter1",
-					"1chapter2",
-					"1chapter3",
-					"1chapter4",
-					"1chapter5",
-				],
-			},
-		],
-	},
-	{
-		id: 3,
-		semSubs: [
-			{
-				SubjectName: ["Physics", "sub2", "sub2", "sub2", "sub2", "sub2"],
-				ChapterName: [
-					"1Chapter1",
-					"1chapter2",
-					"1chapter3",
-					"1chapter4",
-					"1chapter5",
-				],
-			},
-		],
-	},
-	{
-		id: 4,
-		semSubs: [
-			{
-				SubjectName: ["Physics", "sub2", "sub2", "sub2", "sub2", "sub2"],
-				ChapterName: [
-					"1Chapter1",
-					"1chapter2",
-					"1chapter3",
-					"1chapter4",
-					"1chapter5",
-				],
-			},
-		],
-	},
-	{
-		id: 5,
-		semSubs: [
-			{
-				SubjectName: ["Physics", "sub2", "sub2", "sub2", "sub2", "sub2"],
-				ChapterName: [
-					"1Chapter1",
-					"1chapter2",
-					"1chapter3",
-					"1chapter4",
-					"1chapter5",
-				],
-			},
-		],
-	},
-	{
-		id: 6,
-		semSubs: [
-			{
-				SubjectName: ["Physics", "sub2", "sub2", "sub2", "sub2", "sub2"],
-				ChapterName: [
-					"1Chapter1",
-					"1chapter2",
-					"1chapter3",
-					"1chapter4",
-					"1chapter5",
-				],
-			},
-		],
-	},
-	{
-		id: 7,
-		semSubs: [
-			{
-				SubjectName: ["Physics", "sub2", "sub2", "sub2", "sub2", "sub2"],
-				ChapterName: [
-					"1Chapter1",
-					"1chapter2",
-					"1chapter3",
-					"1chapter4",
-					"1chapter5",
-				],
-			},
-		],
-	},
-	{
-		id: 8,
-		semSubs: [
-			{
-				SubjectName: ["Physics", "sub2", "sub2", "sub2", "sub2", "sub2"],
-				ChapterName: [
-					"1Chapter1",
-					"1chapter2",
-					"1chapter3",
-					"1chapter4",
-					"1chapter5",
-				],
-			},
-		],
-	},
-];
+// const subjects = [
+// 	{
+// 		id: 1,
+// 		semSubs: [
+// 			{
+// 				SubjectName: [
+// 					"Physics",
+// 					"Digital logic",
+// 					"Mathematics I",
+// 					"Introduction of Information and technology",
+// 					"C programming",
+// 				],
+// 				ChapterName: [
+// 					"1Chapter1",
+// 					"1chapter2",
+// 					"1chapter3",
+// 					"1chapter4",
+// 					"1chapter5",
+// 				],
+// 			},
+// 		],
+// 	},
+// 	{
+// 		id: 2,
+// 		semSubs: [
+// 			{
+// 				SubjectName: ["Microprocessor", "sub2", "sub2", "sub2", "sub2", "sub2"],
+// 				ChapterName: [
+// 					"1Chapter1",
+// 					"1chapter2",
+// 					"1chapter3",
+// 					"1chapter4",
+// 					"1chapter5",
+// 				],
+// 			},
+// 		],
+// 	},
+// 	{
+// 		id: 3,
+// 		semSubs: [
+// 			{
+// 				SubjectName: ["Physics", "sub2", "sub2", "sub2", "sub2", "sub2"],
+// 				ChapterName: [
+// 					"1Chapter1",
+// 					"1chapter2",
+// 					"1chapter3",
+// 					"1chapter4",
+// 					"1chapter5",
+// 				],
+// 			},
+// 		],
+// 	},
+// 	{
+// 		id: 4,
+// 		semSubs: [
+// 			{
+// 				SubjectName: ["Physics", "sub2", "sub2", "sub2", "sub2", "sub2"],
+// 				ChapterName: [
+// 					"1Chapter1",
+// 					"1chapter2",
+// 					"1chapter3",
+// 					"1chapter4",
+// 					"1chapter5",
+// 				],
+// 			},
+// 		],
+// 	},
+// 	{
+// 		id: 5,
+// 		semSubs: [
+// 			{
+// 				SubjectName: ["Physics", "sub2", "sub2", "sub2", "sub2", "sub2"],
+// 				ChapterName: [
+// 					"1Chapter1",
+// 					"1chapter2",
+// 					"1chapter3",
+// 					"1chapter4",
+// 					"1chapter5",
+// 				],
+// 			},
+// 		],
+// 	},
+// 	{
+// 		id: 6,
+// 		semSubs: [
+// 			{
+// 				SubjectName: ["Physics", "sub2", "sub2", "sub2", "sub2", "sub2"],
+// 				ChapterName: [
+// 					"1Chapter1",
+// 					"1chapter2",
+// 					"1chapter3",
+// 					"1chapter4",
+// 					"1chapter5",
+// 				],
+// 			},
+// 		],
+// 	},
+// 	{
+// 		id: 7,
+// 		semSubs: [
+// 			{
+// 				SubjectName: ["Physics", "sub2", "sub2", "sub2", "sub2", "sub2"],
+// 				ChapterName: [
+// 					"1Chapter1",
+// 					"1chapter2",
+// 					"1chapter3",
+// 					"1chapter4",
+// 					"1chapter5",
+// 				],
+// 			},
+// 		],
+// 	},
+// 	{
+// 		id: 8,
+// 		semSubs: [
+// 			{
+// 				SubjectName: ["Physics", "sub2", "sub2", "sub2", "sub2", "sub2"],
+// 				ChapterName: [
+// 					"1Chapter1",
+// 					"1chapter2",
+// 					"1chapter3",
+// 					"1chapter4",
+// 					"1chapter5",
+// 				],
+// 			},
+// 		],
+// 	},
+// ];
+
+import { semesterDetails } from "../semester/semester.js";
+
 const subjectDiv = document.getElementById("subjectDiv");
 
 function subNameRender(semesterId) {
-	const selectedSemester = subjects.find((sem) => sem.id == semesterId);
-	if (selectedSemester) {
-		selectedSemester.semSubs.forEach((subjects) => {
-			subjects.SubjectName.forEach((SubjectName) => {
+	const semester = semesterDetails.find((sem) => sem.semesterId == semesterId);
+	if (semester) {
+		semester.subjects.forEach((subject) => {
+			subject.chapters.forEach((chapter) => {
 				const listItem = document.createElement("div");
 				let chapterHTML = "";
-				subjects.ChapterName.forEach((ChapterName) => {
-					chapterHTML += `<p class="text-sm">${ChapterName}</p>`;
-				});
+					chapterHTML += `<p class="text-sm">${chapter}</p>`;
 				listItem.innerHTML = `<div class="p-2 rounded-md cursor-pointer bg-blue-200  hover:scale-[1.03] transition-all ease-in-out shadow-xl box2 w-[17rem] h-[15rem]">
-                                          <div class="text-3xl h-[2.4rem] text-ellipsis truncate">${SubjectName}</div>
+                                          <div class="text-3xl h-[2.4rem] text-ellipsis truncate">${chapter}</div>
                                           <div class="h-[64%] p-2">
                                             ${chapterHTML}
                                           </div>
@@ -170,6 +171,6 @@ document.addEventListener("DOMContentLoaded", function () {
 	const semesterItem = document.createElement("p");
 	semesterItem.innerHTML = `<p class="text-3xl p-5">Semester ${semesterId}</p>`;
 	semesteriddiv.appendChild(semesterItem);
-
+    // console.log(semesterId)
 	subNameRender(semesterId);
 });
